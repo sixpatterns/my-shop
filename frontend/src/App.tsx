6d9855a3757807eb58fb448792f5bedb4bbdb4ff
Vivek <vivekmiyaniofficial@gmail.com>
2026-03-21 15:24:17 +0530

    Add react-router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router";
+
+import { Routes } from "./pages/Routes";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/Routes.tsx
@@ -0,0 +1,51 @@
+import { Navigate, Outlet, Route, Routes as Router } from "react-router";
+
+import Layout from "./Layout";
+
+import asyncComponent from "../components/asyncComponent";
+
+const PublicRoute = () => {
+  const session = useSessionsStore((i) => i.session);
+
+  if (session) return <Navigate to="/" />;
+
+  return <Outlet />;
+};
+
+const PrivateRoute = () => {
+  const session = useSessionsStore((i) => i.session);
+
+  if (!session) return <Navigate to="/login" />;
+
+  return <Outlet />;
+};
+
+export const Routes = () => {
+  return (
+    <Router>
+      <Route element={<PublicRoute />}>
+        <Route
+          Component={asyncComponent(() => import("./Login"))}
+          path="/login"
+        />
+      </Route>
+
+      <Route Component={asyncComponent(() => import("./Root"))} path="/" />
+
+      <Route element={<PrivateRoute />}>
+        <Route element={<Layout />}>
+          <Route
+            Component={asyncComponent(() => import("./Dashboard"))}
+            path="/dashboard"
+          />
+          <Route
+            Component={asyncComponent(() => import("./Orders"))}
+            path="/orders"
+          />
+        </Route>
+      </Route>
+
+      <Route element="Not found" path="*" />
+    </Router>
+  );
+};
