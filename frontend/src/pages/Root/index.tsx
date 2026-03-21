7733c823081b564b21da019b2297d88f8cf782f4
Vivek <vivekmiyaniofficial@gmail.com>
2026-03-21 23:30:20 +0530

    Force route replace

--- frontend/src/pages/Root/index.tsx
+++ frontend/src/pages/Root/index.tsx
@@ -5,9 +5,9 @@ import { useSessionStore } from "../../stores/useSessionStore";
 const Root = () => {
   const session = useSessionStore((i) => i.session);
 
-  if (session) return <Navigate to="/dashboard" />;
+  if (session) return <Navigate replace to="/dashboard" />;
 
-  return <Navigate to="/login" />;
+  return <Navigate replace to="/login" />;
 };
 
 export default Root;
