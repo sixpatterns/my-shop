d970fe72f39e8c012599875ce3956e673a2d450f
Vivek <vivekmiyaniofficial@gmail.com>
2026-03-21 15:39:11 +0530

    Install graphql-ruby

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,32 +8,34 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-class Backend::Application < Rails::Application
-  # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 7.2
+module Backend # rubocop:disable Style/ClassAndModuleChildren
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.2
 
-  # Please, add to the `ignore` list any other `lib` subdirectories that do
-  # not contain `.rb` files, or that should not be reloaded or eager loaded.
-  # Common ones are `templates`, `generators`, or `middleware`, for example.
-  config.autoload_lib(ignore: ["assets", "tasks"])
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: ["assets", "tasks"])
 
-  # Configuration for the application, engines, and railties goes here.
-  #
-  # These settings can be overridden in specific environments using the files
-  # in config/environments, which are processed later.
-  #
-  # config.time_zone = "Central Time (US & Canada)"
-  # config.eager_load_paths << Rails.root.join("extras")
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
-  # Only loads a smaller set of middleware suitable for API only apps.
-  # Middleware like session, flash, cookies can be added back manually.
-  # Skip views, helpers and assets when generating a new resource.
-  config.api_only = true
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
 
-  # Override
-  #
-  # UUID primary keys
-  config.generators do |g|
-    g.orm :active_record, primary_key_type: :uuid
+    # Override
+    #
+    # UUID primary keys
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
