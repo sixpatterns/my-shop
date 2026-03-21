5ce4a935fa0191ffb0c2bd9aec113d42a5a7acb5
Vivek <vivekmiyaniofficial@gmail.com>
2026-03-21 14:45:34 +0530

    Default to UUID primary keys

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,4 +29,11 @@ class Backend::Application < Rails::Application
   # Middleware like session, flash, cookies can be added back manually.
   # Skip views, helpers and assets when generating a new resource.
   config.api_only = true
+
+  # Override
+  #
+  # UUID primary keys
+  config.generators do |g|
+    g.orm :active_record, primary_key_type: :uuid
+  end
 end
