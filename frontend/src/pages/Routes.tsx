40e6f71b7c237e02d712a30dd619eaf3c39ba903
Vivek <vivekmiyaniofficial@gmail.com>
2026-04-01 13:58:16 +0530

    Define async component at module level

--- frontend/src/pages/Routes.tsx
+++ frontend/src/pages/Routes.tsx
@@ -2,8 +2,13 @@ import { Navigate, Outlet, Route, Routes as Router } from "react-router";
 
 import Layout from "./Layout";
 
-import asyncComponent from "../components/asyncComponent";
+import AsyncComponent from "../components/AsyncComponent";
 import { useSessionStore } from "../stores/useSessionStore";
+
+const Dashboard = AsyncComponent(() => import("./Dashboard"));
+const Login = AsyncComponent(() => import("./Login"));
+const Orders = AsyncComponent(() => import("./Orders"));
+const Root = AsyncComponent(() => import("./Root"));
 
 const PublicRoute = () => {
   const session = useSessionStore((i) => i.session);
@@ -25,24 +30,15 @@ export const Routes = () => {
   return (
     <Router>
       <Route element={<PublicRoute />}>
-        <Route
-          Component={asyncComponent(() => import("./Login"))}
-          path="/login"
-        />
+        <Route Component={Login} path="/login" />
       </Route>
 
-      <Route Component={asyncComponent(() => import("./Root"))} path="/" />
+      <Route Component={Root} path="/" />
 
       <Route element={<PrivateRoute />}>
         <Route element={<Layout />}>
-          <Route
-            Component={asyncComponent(() => import("./Dashboard"))}
-            path="/dashboard"
-          />
-          <Route
-            Component={asyncComponent(() => import("./Orders"))}
-            path="/orders"
-          />
+          <Route Component={Dashboard} path="/dashboard" />
+          <Route Component={Orders} path="/orders" />
         </Route>
       </Route>
 
